7f40db53fe502af50705b46e316fd2012b4bd68f
David Lumley <djlumley@gmail.com>
2012-12-31 21:13:19 +1000

    update spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,18 @@ ENV['RACK_ENV'] = 'test'
 
 require File.expand_path('../../config/boot', __FILE__)
 
+def session
+  last_request.env['rack.session']
+end
+
+def form_errors
+	last_request.env['rack.session'][:__FORMERRORS__] || {}
+end
+
+def json_response
+	JSON.parse last_response.body
+end
+
 RSpec.configure do |config|
 	config.include(DataMapper::Matchers)
 	config.before(:each) { DataMapper.auto_migrate! }
