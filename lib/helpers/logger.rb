49774f309877eedefd548e4d80d1b9ee39e7b46b
David Lumley <djlumley@gmail.com>
2013-01-20 19:14:32 +1000

    move helper to Application::Helpers

--- lib/helpers/logger.rb
+++ lib/helpers/logger.rb
@@ -4,18 +4,30 @@
 # logger[:database].fatal	"can't connect to mongodb @ 10.10.10.52"
 #
 
+module Application
+	module Helpers
+
+		class Logger
+			def logger(type = :application)
+				unless custom_logger = Log4r::Logger[type.to_s]
+					custom_logger = Log4r::Logger.new type.to_s
+					custom_logger.add Log4r::FileOutputter.new(type.to_s, {
+						:filename => "log/#{ENV['RACK_ENV']}_#{type}.log",
+					})
+					custom_logger.level = Log4r.const_get(ENV['LOGGER_LEVEL'])
+				end
+				return custom_logger
+			end
+		end
+
+	end
+end
+
 module Sinatra
 	module Helpers
 		
 		def logger(type = :application)
-			unless custom_logger = Log4r::Logger[type.to_s]
-				custom_logger = Log4r::Logger.new type.to_s
-				custom_logger.add Log4r::FileOutputter.new(type.to_s, {
-					:filename => "log/#{ENV['RACK_ENV']}_#{type}.log",
-				})
-				custom_logger.level = Log4r.const_get(ENV['LOGGER_LEVEL'])
-			end
-			return custom_logger
+			(@logger ||= Application::Helpers::Logger.new).logger(type)
 		end
 		
 	end
